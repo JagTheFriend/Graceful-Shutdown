efacf8fd9eec2e745c6143475552ca7ce1954ca3
JagTheFriend <JagTheFriend12@gmail.com>
2025-11-02 13:04:11 +0530

    Enhance README and main.go: add signal handling, slow request simulation, and graceful shutdown functionality
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- README.md
+++ README.md
@@ -5,6 +5,21 @@
 This is a simple web server written in Go that demonstrates graceful shutdown.
 It listens for OS signals (like SIGINT and SIGTERM) and performs cleanup operations before exiting.
 
+
+## Signal Handling
+
+The application listens for OS signals (SIGINT and SIGTERM) to perform cleanup operations before exiting.
+
+### Handling Signals
+
+When the application receives SIGINT/SIGTERM, it will perform the following cleanup operations:
+
+1. Log a message indicating that the server is shutting down.
+2. Send a SIGINT signal to the server's goroutine to gracefully shutdown the server.
+3. Wait for the server's goroutine to finish shutting down.
+4. Log a message indicating that the server has shut down.
+5. Exit the application.
+
 ## Usage
 
 To run the application, execute the following command:
@@ -14,4 +29,12 @@ go run ./cmd/main.go
 ```
 
 The application will start and listen on port 8000.\
-You can access the application by opening a web browser and navigating to `http://localhost:8000`.
+You can access the application by opening a web browser and navigating to `http://localhost:8000/example`.
+
+This route will simulate a slow request that takes 8 seconds to complete.
+
+### Testing Graceful Shutdown
+
+To test graceful shutdown, you can send a SIGINT signal to the application.\
+This will cause the application to perform the cleanup operations and exit.\
+You can send a SIGINT signal by pressing `Ctrl+C` in the terminal where the application is running.\
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,96 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func createServer() *http.Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/example", func(w http.ResponseWriter, r *http.Request) {
+		log.Default().Println("Slow request started")
+		time.Sleep(8 * time.Second)
+		log.Default().Println("Slow request finished")
+		_, err := w.Write([]byte("Hello, World!"))
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	})
+
+	return &http.Server{
+		Addr:    ":8000",
+		Handler: mux,
+	}
+}
+
+func runServer(
+	ctx context.Context,
+	server *http.Server,
+	shutdownDuration time.Duration,
+) error {
+	// Use buffered channel to handle server shutdown errors and not block main thread
+	serverError := make(chan error, 1)
+
+	// Start server in go route to not block
+	go func() {
+		log.Default().Println("Starting server on", server.Addr)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			// Send error to channel if server fails
+			serverError <- err
+		}
+		close(serverError)
+	}()
+
+	// Listen for termination signal to gracefully shutdown the server
+	stopSignal := make(chan os.Signal, 1)
+	signal.Notify(stopSignal, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(stopSignal)
+	defer close(stopSignal)
+
+	select {
+	case err := <-serverError:
+		return err
+
+	case <-stopSignal:
+		log.Default().Println("Shutting down server -- Received termination signal")
+		return shutDownServer(server, shutdownDuration)
+
+	case <-ctx.Done():
+		log.Default().Println("Shutting down server -- Received context cancellation")
+		return shutDownServer(server, shutdownDuration)
+	}
+}
+
+func shutDownServer(server *http.Server, shutdownDuration time.Duration) error {
+	// Create context with timeout for server shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownDuration)
+	defer cancel()
+
+	// Attempt graceful server shutdown and fallback to hard close if it fails
+	if shutdownErr := server.Shutdown(ctx); shutdownErr != nil {
+		if closeErr := server.Close(); closeErr != nil {
+			return errors.Join(shutdownErr, closeErr)
+		}
+	}
+	log.Default().Println("Server gracefully stopped")
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := createServer()
+	shutdownDuration := 5 * time.Second
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := runServer(ctx, server, shutdownDuration); err != nil {
+		log.Fatal(err.Error())
+	}
 }
